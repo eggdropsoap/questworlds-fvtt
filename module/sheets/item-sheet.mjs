6d9046c9d1e70563a437858f4fe19c64be1c129b
eggdropsoap <eggdropsoap@pipemaze.com>
2021-07-31 10:05:06 -0700

    Begin switch to per-type item sheets

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -18,11 +18,11 @@ export class QuestWorldsItemSheet extends ItemSheet {
   get template() {
     const path = "systems/questworlds/templates/item";
     // Return a single sheet for all item types.
-    return `${path}/item-sheet.html`;
+    //return `${path}/item-sheet.html`;
 
     // Alternatively, you could use the following return statement to do a
     // unique item sheet by type, like `weapon-sheet.html`.
-    // return `${path}/item-${this.item.data.type}-sheet.html`;
+   return `${path}/item-${this.item.data.type}-sheet.html`;
   }
 
   /* -------------------------------------------- */
