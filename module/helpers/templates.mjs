a1d54594408b8fe906bbfc54ecabd37dcde5a264
eggdropsoap <eggdropsoap@pipemaze.com>
2021-09-04 13:47:53 -0700

    Variant picker in ability sheet settings tab

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -11,8 +11,12 @@
     "systems/questworlds/templates/actor/parts/actor-abilities-ability.html",
     "systems/questworlds/templates/actor/parts/actor-abilities-keyword.html",
     "systems/questworlds/templates/actor/parts/actor-abilities-sidekick.html",
+    "systems/questworlds/templates/actor/parts/actor-abilities-magicgroup.html",
     "systems/questworlds/templates/actor/parts/actor-benefits.html",
     "systems/questworlds/templates/actor/parts/actor-flaws.html",
     //"systems/questworlds/templates/actor/parts/actor-effects.html",
+
+    // Item partials
+    "systems/questworlds/templates/item/parts/item-ability-settings.html",
   ]);
 };
