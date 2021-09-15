3e4f3a1b530af66ef6dcf3da46e143cf422e295a
eggdropsoap <eggdropsoap@pipemaze.com>
2021-09-14 19:31:52 -0700

    Comment clarity

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -140,7 +140,7 @@ export class QuestWorldsItem extends Item {
       // if found (not null) push into nested list
       if (parent) parent.embeds.push(newBreakout);
     }
-    else {
+    else { // it's an Item, not an embed
       // push new sub-ability directly into the item's top-level embeds array
       embeds.push(newBreakout);
     }
