f91abda4366bf4d76f3adff7b7ece558362ad9ef
eggdropsoap <eggdropsoap@pipemaze.com>
2021-09-20 11:54:38 -0700

    Rough-in font filepicker.

--- module/documents/rune-settings-menu.mjs
+++ module/documents/rune-settings-menu.mjs
@@ -24,6 +24,8 @@ export class RuneFontsSettingsMenuClass extends FormApplication {
     }
 
     getData() {
+        console.log("getData() fired");
+
         const settingData = game.settings.get('questworlds', 'runeFontSettings');
         // const settingData = {};   // DEBUG: Clear settings. If paired with .set(), wipes stored settings too!
         
@@ -56,7 +58,7 @@ export class RuneFontsSettingsMenuClass extends FormApplication {
         if (!settingData.hasOwnProperty('fonts')) settingData.fonts = [];
         // vvv stub for development testing! remove! vvv
         // settingData.fonts = [];
-        settingData.fonts = [ { name: "Hello", file: "" } ];
+        // settingData.fonts = [ { name: "Hello", file: "" } ];
         // settingData.fonts = [ { name: "Hello", file: "" }, { name: "Goodbye", file: "" } ];
 
         /* once we have settings data or default data or both merged, normalize data for the form */
@@ -81,12 +83,14 @@ export class RuneFontsSettingsMenuClass extends FormApplication {
         // add convenience data to the context
         settingData.fontCount = masterFontCount;
 
+
+        // console.log(this.filepickers);
         console.log(settingData);   // DEBUG: for inspecting data structure sent to form
         return settingData;
     }
 
     _updateObject(event, formData) {
-        // console.log("_updateObject() fired")
+        console.log("_updateObject() fired")
         const data = expandObject(formData);
         // console.log(data);
         
@@ -111,17 +115,71 @@ export class RuneFontsSettingsMenuClass extends FormApplication {
             }
             // console.log(rune);
         }
-        // console.log(data);
 
-        // game.settings.set('questworlds', 'runeFontSettings', data).then(() => {this.render(true)});
+        /* handle fonts inputs */
+
+        // if no font data from form, make sure .fonts exists
+        if (!data.hasOwnProperty('fonts')) data.fonts = [];
+
+        // if font data from form, it'll be an object, so reserialize as array
+        console.log("It's an Object. Converting to array");
+        data.fonts = Object.values(data.fonts);
+        // console.log(Object.values(data.fonts));
+
+        // if a new font file was uploaded or selected, process and push into .fonts array
+        if (data.newfont.url) {
+            console.log("New font detected");
+            console.log(data.newfont);
+            let newFont = data.newfont;
+            newFont.name = newFont.url;
+
+            data.fonts.push(newFont);
+            delete data.newfont;
+        }
+
+        console.log(data);
+
+        game.settings.set('questworlds', 'runeFontSettings', data).then(() => {this.render(true)});
 
     }
 
-    // /** @override */
-    // async _onSubmit(event, updateData, preventClose, preventRender) {
-    //     console.log('_onSumbit() fired');
-    //     console.log(this);
-    //     return super._onSubmit(event, updateData, preventClose, false);
-    // }
+    /** @override */
+    activateListeners(html) {
+        super.activateListeners(html)
+
+        for ( let fpbutton of html.find('button.font-file-picker') ) {
+            fpbutton.onclick = event => {
+                event.preventDefault();
+
+                const options = this._getFilePickerOptions(event);
+                // console.log("FP options");
+                options.callback = () => {this.submit();}
+                // console.log(options);
+                const fp = new FontFilePicker(options);
+                this.filepickers.push(fp);
+
+                fp.browse();
+                // console.log(fp);
+                // console.log(this);
+                // TODO: start saving data and confirm newfont.url is targeted right
+
+            };
+        }
+    }
+
+}
+
+
+/* support font file extensions */
+class FontFilePicker extends FilePicker {
+    constructor(options={}) {
+        super(options);
+        const FONT_FILE_EXTENSIONS = [
+            "ttf",
+            "otf",
+        ]
+        // TODO: Actually use the font file extensions constant
+        this.extensions = [".ttf"];
+    }
 
 }
